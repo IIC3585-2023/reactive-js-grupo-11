894c5f8faaefb5deb484bf6554312f084a80d118
clementesepulveda <clemente.seplveda@uc.cl>
2023-04-08 11:10:04 -0400

    juego con movimiento

---
+++ constants.js
@@ -0,0 +1,21 @@
+export const TILE_SIZE = 40;
+
+export const DIRS = {
+    "w": [0, -1],
+    "a": [-1, 0],
+    "s": [0, 1],
+    "d": [1, 0],
+}
+
+export const SPEED = 1;
+
+export const LEVEL = [
+    ["w","w","w","w","w","w","w","w"],
+    ["w","f","f","f","f","f","w","w"],
+    ["w","f","f","f","f","f","f","w"],
+    ["w","f","f","f","f","f","f","w"],
+    ["w","f","w","w","w","f","f","f"],
+    ["w","f","-","w","f","f","f","w"],
+    ["w","w","-","-","-","w","w","w"],
+    ["w","w","w","w","w","w","w","w"],
+]
